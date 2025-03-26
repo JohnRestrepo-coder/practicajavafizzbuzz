19729310b544b6cad58bc2545ed798c6b8e1278b
JohnRestrepo-coder <john.restrepo.aparicio@correounivalle.edu.co>
2025-03-26 12:21:13 -0500

    Primer commit

---
+++ task1.js
@@ -0,0 +1,10 @@
+// Refer to Task 1 in your Instructions to complete this task
+//let numeros = Array.from({ length: 106 }, (_, i) => i + 1);
+//
+//for (let i = 0; i < 106; i++) {
+//  console.log("This is Task One!", i); 
+//}
+//
+for (let i = 1; i <= 105; i++) {
+  console.log(i);
+}
